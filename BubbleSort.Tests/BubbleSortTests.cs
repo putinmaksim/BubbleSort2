490893556e0caadb1099b183b41de41cdde2e80c
putinmaksim <38439870+putinmaksim@users.noreply.github.com>
2018-04-25 20:04:19 +0500

    Update BubbleSortTests.cs

--- BubbleSort.Tests/BubbleSortTests.cs
+++ BubbleSort.Tests/BubbleSortTests.cs
@@ -28,17 +28,17 @@ namespace BubbleSort.Tests
         [TestMethod]
         public void TestSimple()//тест обычного массива
         {
-            int[] Arr1 = new int[8] {0, 2, 3, 7, 6, 1, 5, 4};
+            int[] Arr1 = new int[8] {10, 2, 3, 8, 1, 7, 6, 4};
             sorter.BubSort(Arr1);
-            int[] Exp = new int[8] {0, 1, 2, 3, 4, 5, 6, 7, };
+            int[] Exp = new int[8] {1, 2, 3, 4, 6, 7, 8, 10, };
             CollectionAssert.AreEqual(Exp, Arr1);
         }
         [TestMethod]
         public void TestMinus()//тест массива с отрицательными элементами
         {
-            int[] Arr1 = new int[5] { 2, -1, 1, 0, -2 };
+            int[] Arr1 = new int[5] { 4, -5, 2, 0, -2 };
             sorter.BubSort(Arr1);
-            int[] Exp = new int[5] { -2, -1, 0, 1, 2 };
+            int[] Exp = new int[5] { -5, -2, 0, 2, 4 };
             CollectionAssert.AreEqual(Exp, Arr1);
         }
         [TestMethod]
